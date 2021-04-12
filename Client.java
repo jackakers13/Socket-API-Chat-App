c6db981a91e1af86132e37fbffa0c54e52f6df44
jackakers13 <jackakers13@gmail.com>
2021-04-12 11:30:19 -0500

    Implement Simple Client->Server Packet

--- Client.java
+++ Client.java
@@ -7,19 +7,33 @@
  */
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
 
+        // Configuration Variables
         String hostname = "localhost";
         int portnumber = 4999;
 
-        Socket socket;
+        // Variables
+        Socket socket = null;
+
+        // Create Socket Connection w/ Server
         try {
             socket = new Socket(hostname, portnumber);
         } catch (IOException exception) {
-            System.err.printf("[Error] Failed to create socket with host \"%s\" on port \"%d\"", hostname, portnumber);
+            System.err.printf("[Error] Failed to create socket with host \"%s\" on port \"%d\"\n", hostname, portnumber);
+        }
+
+        // Send Message
+        try {
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            writer.println("Hello, I'm a Client!");
+            writer.flush();
+        } catch (IOException exception) {
+            System.err.printf("[Error] Failed to send message!\n");
         }
         
     }
--- Server.java
+++ Server.java
@@ -6,6 +6,8 @@
  * Implemented using Java 8; built and tested on Windows 10.
  */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,19 +15,32 @@ import java.net.Socket;
 public class Server {
     public static void main(String[] args) {
 
+        // Configuration Variables
         int portnumber = 4999;
 
-        ServerSocket serverSocket;
-        Socket socket;
+        // Variables
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        InputStreamReader inputStream = null;
+        BufferedReader reader = null;
 
+        // Accept Socket Connection from Client
         try {
             serverSocket = new ServerSocket(4999);
             socket = serverSocket.accept();
+            inputStream = new InputStreamReader(socket.getInputStream());
+            reader = new BufferedReader(inputStream);
+            System.out.println("Client Connected!");
         } catch (IOException exception) {
-            System.err.printf("[Error] Failed to create socket on port \"%d\"", portnumber);
+            System.err.printf("[Error] Failed to create socket on port \"%d\"\n", portnumber);
         }
 
-        System.out.println("Client Connected!");
-        
+        // Recieve Message
+        try {
+            System.out.printf("Recieved \"%s\" from client.\n", reader.readLine());
+        } catch (IOException exception) {
+            System.err.printf("[Error] Failed to read from BufferedReader!\n");
+        }
+
     }
 }
